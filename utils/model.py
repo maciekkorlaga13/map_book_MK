92398a585aaf67cf7dedd6b42eb7c55db9ae50eb
maciekkorlaga13 <maciek.korlaga13@gmail.com>
2025-04-15 19:10:42 +0200

    wip: update user functionality

--- utils/model.py
+++ utils/model.py
@@ -1,6 +1,6 @@
 users: list = [
     {'name': 'Maciej', 'location': 'Łódź', 'posts': 100},
     {'name': 'Mateusz', 'location': 'Poznań', 'posts': 200},
-    {'name': 'Maciej', 'location': 'Siedlce', 'posts': 300},
+    {'name': 'Maciej01', 'location': 'Siedlce', 'posts': 300},
     {'name': 'Konrad', 'location': 'Grudziądz', 'posts': 400},
 ]
